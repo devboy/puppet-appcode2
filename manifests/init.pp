c131e3583fe0be549a998cc7b19e3ac432ba4dfd
Dominic Graefen <dominic.graefen@gmail.com>
2013-03-20 14:52:23 +0100

    Changes to release version of AppCode2

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,20 @@
+# Public: AppCode 2 by JetBrains
+#
+# Examples
+#
+# include appcode2
+# 
+# or
+#
+# class {'appcode2':
+#   version => 2.0, 
+# }
 class appcode2 (
-  $build = '129.41',
+  $version = '2.0',
 ) {
   require java
   package { 'AppCode2':
     provider => 'appdmg',
-    source   => "http://download.jetbrains.com/objc/AppCode-${build}.dmg",
+    source   => "http://download.jetbrains.com/objc/AppCode-#{version}.dmg",
   }
 }
